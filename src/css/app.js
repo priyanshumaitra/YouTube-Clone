de77e2b3b5ad368c059fe54c3b1be904a02c8acd
priyanshu <priyanshumaitra@gmail.com>
2022-10-20 19:10:27 +0530

    reverted to the old sidebar scroll

--- src/css/app.js
+++
@@ -1,9 +0,0 @@
-const scrollbar = document.querySelector(".sidebar")
-scrollbar.addEventListener("mouseenter",()=>{
-    scrollbar.classList.toggle("scroll");
-
-})
-scrollbar.addEventListener("mouseleave",()=>{
-    scrollbar.classList.toggle("scroll");
-
-})
